3563e1d8e06a255482e0987b131a65248b14e829
fazilraza5233 <fazilraza5233@gmail.com>
2025-11-10 12:22:10 +0000

    Added

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success("✅ Order submitted successfully!")
 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
